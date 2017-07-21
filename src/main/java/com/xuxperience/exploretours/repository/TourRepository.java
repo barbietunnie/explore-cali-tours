80cd89d8d35464c86b81c99d80bda7a6ab8b0b3f
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-07-21 09:17:26 +0100

    Make TourRepository pageable and sortable

--- src/main/java/com/xuxperience/exploretours/repository/TourRepository.java
+++ src/main/java/com/xuxperience/exploretours/repository/TourRepository.java
@@ -1,7 +1,10 @@
 package com.xuxperience.exploretours.repository;
 
 import com.xuxperience.exploretours.entity.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +14,6 @@ import java.util.List;
  * Tour Repository Interface
  */
 @Repository
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(@Param("code") String code);
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 }
