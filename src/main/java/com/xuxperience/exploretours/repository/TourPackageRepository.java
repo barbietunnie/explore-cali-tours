4a43feecb43c1b7b34a60fe4816d3e075cf7cbf3
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-07-21 09:23:02 +0100

    Prevent access to some endpoints and rename some paths

--- src/main/java/com/xuxperience/exploretours/repository/TourPackageRepository.java
+++ src/main/java/com/xuxperience/exploretours/repository/TourPackageRepository.java
@@ -4,6 +4,8 @@ import com.xuxperience.exploretours.entity.Tour;
 import com.xuxperience.exploretours.entity.TourPackage;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +13,32 @@ import java.util.List;
 /**
  * Tour Package Repository Interface
  */
-@Repository
+//@Repository
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
     TourPackage findByName(@Param("name") String name);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> save(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage tourPackage);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Iterable<? extends TourPackage> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
