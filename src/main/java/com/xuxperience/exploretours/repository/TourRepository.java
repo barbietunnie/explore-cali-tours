6d1e8ba7e325108155d9de0aaad14247001def7d
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-07-21 08:58:14 +0100

    Add @Param to TourRepository to enable search

--- src/main/java/com/xuxperience/exploretours/repository/TourRepository.java
+++ src/main/java/com/xuxperience/exploretours/repository/TourRepository.java
@@ -2,6 +2,7 @@ package com.xuxperience.exploretours.repository;
 
 import com.xuxperience.exploretours.entity.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.List;
  */
 @Repository
 public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(String code);
+    List<Tour> findByTourPackageCode(@Param("code") String code);
 }
